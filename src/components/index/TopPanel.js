ec17976be296190cdc24bc589127cf2f08c7cfd2
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-07 11:17:15 +0000

    add top panel component and html constructor

---
+++ src/components/index/TopPanel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class IndexTopPanel extends Component {
+  render(){
+    return (
+      <div className="IndexTopPanel" id="IndexTopPanel">
+        <a> Ryan Clark </a>
+        <a> Phone: +1-210-294-1400 </a>
+        <a> Email: wolfen7lada@gmail.com </a>
+        <a> Full Stack Developer </a>
+      </div>
+    )
+  }
+}
+
+export default IndexTopPanel;
