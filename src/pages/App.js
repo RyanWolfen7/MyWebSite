dd54816a4b6e98edb43927a9ce37633eca78ce2c
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-07 11:16:30 +0000

    added top panel component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import IndexTopPanel from '../components/index/TopPanel.js'
 
 class App extends Component {
 
@@ -6,7 +7,7 @@ class App extends Component {
 
     return (
       <div className="app">
-
+        <IndexTopPanel/>
       </div>
     )
   }
