8826da2320bc9a9586d3406f6c4179087b41fee7
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-10 15:15:20 +0000

    added Index BG div

--- src/components/index/TopPanel.js
+++ src/components/index/TopPanel.js
@@ -4,8 +4,10 @@ class IndexTopPanel extends Component {
   render(){
     return (
       <div className="IndexTopPanel" id="IndexTopPanel">
-        <a> Ryan Clark </a>
-        <a> Full Stack Developer </a>
+        <div className="IndexTopPanel-BG" id="IndexTopPanel-BG"
+          <a> Ryan Clark </a>
+          <a> Full Stack Developer </a>
+        </div>
       </div>
     )
   }
