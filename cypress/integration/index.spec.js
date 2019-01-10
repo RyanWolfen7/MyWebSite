5e142da9e1bde353e64517e1ae04604a28bcfdf9
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-10 15:40:46 +0000

    redirecting approach and design

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -5,15 +5,4 @@ describe('Index Page', function() {
   it('App Launches and can visit the website', function() {
     cy.visit('http://localhost:3000')
   })
-  it('Has Programers Name, Title', function() {
-    cy.get('.IndexTopPanel')
-      .should('be.visible')
-      .and('contain', 'Ryan Clark')
-      .and('contain', 'Full Stack Developer')
-  })
-  it(' Has Background Div class'), function () {
-    cy.get('.IndexTopPanel-BG')
-      .should('have.css', 'background-color')
-      .and('eq', 'rgb(107, 142, 35)')
-  }
 })
--- src/components/index/TopPanel.js
+++ src/components/index/TopPanel.js
@@ -4,10 +4,6 @@ class IndexTopPanel extends Component {
   render(){
     return (
       <div className="IndexTopPanel" id="IndexTopPanel">
-        <div className="IndexTopPanel-BG" id="IndexTopPanel-BG">
-          <a> Ryan Clark </a>
-          <a> Full Stack Developer </a>
-        </div>
       </div>
     )
   }
