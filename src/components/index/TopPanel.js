edc6ff17cdb642e53513e54f7a240f636fe05c8a
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-11 14:13:19 +0000

    added CSS styling

--- src/components/index/TopPanel.js
+++ src/components/index/TopPanel.js
@@ -4,6 +4,10 @@ class IndexTopPanel extends Component {
   render(){
     return (
       <div className="IndexTopPanel" id="IndexTopPanel">
+        <div className="Header" id="Header">
+          <div className="RyanClark" id="RyanClark"> Ryan Clark </div>
+          <div className="Profession" id="Profession"> Full Stack Web Designer </div>
+        </div>
       </div>
     )
   }
