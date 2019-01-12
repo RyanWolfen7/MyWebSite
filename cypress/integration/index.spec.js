df763171f2759cbfe94aac92ed031a129b373bae
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-12 22:13:31 +0000

    fixed tests for the initial page

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -5,4 +5,16 @@ describe('Index Page', function() {
   it('App Launches and can visit the website', function() {
     cy.visit('http://localhost:3000')
   })
+  it('Should load my name', function() {
+    cy.get('.RyanClark')
+      .should('contain', 'Ryan Clark')
+  })
+  it('Should load my job title', function() {
+    cy.get('.Profession')
+    .should('contain', 'Full Stack Web Developer')
+  })
+  it('Should load my picture', function() {
+    cy.get('.Me')
+    .should('exist', 'ME.jpg')
+  })
 })
