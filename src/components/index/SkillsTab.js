e19cddb0217778f32bedcdde538b9e456ff900f0
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-18 18:00:34 +0000

    added skills and leadership description

--- src/components/index/SkillsTab.js
+++ src/components/index/SkillsTab.js
@@ -6,7 +6,7 @@ class SkillsTab extends Component {
   render(){
     return (
       <div className="skills" id="skills">
-        <Menu customBurgerIcon={ <img src={ Skills } alt="Skills"/>} width={ '.02vw'} left>
+        <Menu customBurgerIcon={ <img src={ Skills } width={'10vw'} alt="Skills"/>} right>
           <SkillsList/>
         </Menu>
       </div>
@@ -17,8 +17,19 @@ class SkillsTab extends Component {
 const SkillsList = () => {
   return (
       <div className="skillsList" id= "skillsList">
-        Leadership
-        
+        <h1> Leadership </h1>
+        <p>
+          From the Jungles of Southeast Asia, to the computer labs of London,
+          Ryan has lead teams of programers, teams of salesmen, and teams of infantrymen.
+          Taught leadership from some of the best institutions in the United States Army,
+          With over 10 years experience Ryan takes pride in his versitile and adaptive
+          leadership styles.
+        </p>
+        <h1> Agile Methodology </h1>
+        <h1> Self Learning</h1>
+        <h1> Test Driven Development</h1>
+        <h1> Object Oriented Design</h1>
+        <h1> Domain Driven Design</h1>
       </div>
   );
 };
