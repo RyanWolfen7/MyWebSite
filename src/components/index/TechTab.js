edc4370a5a470444d7ae45167acbf560de101532
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-27 22:20:28 +0000

    updated component with languages

--- src/components/index/TechTab.js
+++ src/components/index/TechTab.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import Tech from '../../assets/images/Tech.jpg'
+import Tech from '../../assets/images/Tech.jpg';
+import js from '../../assets/images/languages/JS.png';
+import ruby from '../../assets/images/languages/Ruby.jpg';
+import css from '../../assets/images/languages/css.png';
+import html from '../../assets/images/languages/html.png';
+import sql from '../../assets/images/languages/sql.jpg';
+import c from '../../assets/images/languages/C.png';
 
 class TechTab extends Component {
   render(){
@@ -16,7 +22,32 @@ class TechTab extends Component {
 
 const TechList = () => {
   return(
-    <a>test</a>
+    <div className="tech" id="tech">
+      <Proficient/>
+      <Functional/>
+    </div>
+  );
+};
+
+const Proficient = () => {
+  return (
+    <div className="proficient" id="proficient">
+      <h1> Proficient Languages </h1>
+      <img src={ ruby } alt="Ruby" className="ruby" />
+      <img src={ js } alt="JS" className="js" />
+      <img src={ html } alt="html" className="html" />
+      <img src={ css } alt="css" className="css" />
+      <img src={ sql } alt="sql" className="sql" />
+    </div>
+  );
+};
+
+const Functional = () => {
+  return (
+    <div className="functional" id="functional">
+      <h1> Language Familiarities </h1>
+      <img src={ c } alt="c" className="c" />
+    </div>
   );
 };
 
